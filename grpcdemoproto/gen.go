1f3d49a02ff98a7c563a7136200f04e1f9533e91
troylelandshields <troy.leland.shields@gmail.com>
2016-11-01 16:47:50 -0600

    Vendor dependencies.

--- grpcdemoproto/gen.go
+++ grpcdemoproto/gen.go
@@ -1,3 +1,3 @@
 package grpcdemoproto
 
-//go:generate protoc -I=./ -I=$GOPATH/src --go_out=plugins=grpc:$GOPATH/src grpcdemo.proto
+//go:generate protoc -I=./ -I=$GOPATH/src/github.com/weave-lab/grpc-demo/vendor --go_out=plugins=grpc:$GOPATH/src grpcdemo.proto
